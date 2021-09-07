2536782b9b0eb44ee918718bd2370ba5956e8ab0
rony <rriachi@bright-lab.com>
2021-09-07 17:29:31 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,17 @@ function Navbar() {
     <div >
         <div className="apple">
 
+            
 
-            <p className = "text-white inline">Lpardo</p>
-            <ul className = "float-right">
-                <li className = "text-white inline"><a href="#">Home</a></li>
-                <li className = "text-white ml-4 inline"><a href="#">Pages</a></li>
-                <li className = "text-white ml-4 inline"><a href="#">About</a></li>
-                <li className = "text-white ml-4 inline"><a href="#">Blog</a></li>
-                <li className = "text-white ml-4 inline"><a href="#">Contacts</a></li>
-                <li className = "text-white ml-4 inline"><a href="#">Contacts</a></li>
+
+            <p className = "text-white inline ml-6 mt-4">Lpardo</p>
+            <ul className = "float-right mt-4 mr-2">
+                <li className = "text-white inline text-sm"><a href="#">Home</a></li>
+                <li className = "text-white ml-4 inline text-sm"><a href="#">Pages</a></li>
+                <li className = "text-white ml-4 inline text-sm"><a href="#">About</a></li>
+                <li className = "text-white ml-4 inline text-sm"><a href="#">Blog</a></li>
+                <li className = "text-white ml-4 inline text-sm"><a href="#">Contacts</a></li>
+                <li className = "text-white ml-4 inline text-sm"><a href="#">Contacts</a></li>
             </ul>
 
         <div className = "text-white text-5xl transform translate-y-12 translate-x-32">About Us</div>
