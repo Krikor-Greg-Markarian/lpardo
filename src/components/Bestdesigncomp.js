bbb0330eee27b77b0026618ed2b3a6aa68ce5520
rony <rriachi@bright-lab.com>
2021-09-08 15:09:07 +0300

    some title with subtitles

--- src/components/Bestdesigncomp.js
+++ src/components/Bestdesigncomp.js
@@ -4,7 +4,7 @@ import { FaSmile } from "react-icons/fa";
 function Bestdesigncomp(props) {
   return (
     <div className = " mt-3">
-        <div className = "image"></div>
+        <div className = ""></div>
         <p className = "font-bold pb-4 ">{props.title} </p>
         <p className = "text-sm text-gray-500">{props.description}</p>
     </div>
