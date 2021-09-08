475e1a670f89ee06e6664f53d9b7e5cd4329ad2d
rony <rriachi@bright-lab.com>
2021-09-08 15:58:34 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,34 +3,52 @@ import { FaSearch } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div >
-        <div className="apple">
-
-            
-
-
-            <p className = "text-white inline ml-6 mt-4">Lpardo</p>
-            <ul className = "float-right mt-4 mr-2">
-                <li className = "text-white inline text-sm"><a href="#">Home</a></li>
-                <li className = "text-white ml-4 inline text-sm"><a href="#">Pages</a></li>
-                <li className = "text-white ml-4 inline text-sm"><a href="#">About</a></li>
-                <li className = "text-white ml-4 inline text-sm"><a href="#">Blog</a></li>
-                <li className = "text-white ml-4 inline text-sm"><a href="#">Contacts</a></li>
-                <li className = "text-white ml-4 inline text-sm"><a href="#">Contacts</a></li>
+    <div>
+      <div className="apple">
+        <div className="p-4">
+          <div className="text-white inline">
+            Lpardo
+            <ul className="inline float-right">
+              <li className="text-white inline text-sm">
+                <a href="#">Home</a>
+              </li>
+              <li className="text-white ml-4 inline text-sm">
+                <a href="#">Pages</a>
+              </li>
+              <li className="text-white ml-4 inline text-sm underline">
+                <a href="#">About</a>
+              </li>
+              <li className="text-white ml-4 inline text-sm">
+                <a href="#">Blog</a>
+              </li>
+              <li className="text-white ml-4 inline text-sm">
+                <a href="#">Contacts</a>
+              </li>
+              <li className="text-white ml-4 inline text-sm">
+                <a href="#">Contacts</a>
+              </li>
             </ul>
-
-        <div className = "text-white text-5xl transform translate-y-12 translate-x-32">About Us</div>
-        <div className = "">
-            <ul className = "transform translate-y-12 translate-x-32">
-                <li className = "text-white inline ml-2"><a href="#">HOME /</a></li>
-                <li className = "text-white inline ml-2"><a href="#">PAGES /</a></li>
-                <li className = "text-white inline ml-2"><a href="#">ABOUT US</a></li>
-            </ul>
+            <div className="p-9">
+              <div className="text-white text-5xl transform translate-y-12 translate-x-32 ">
+                About Us
+              </div>
+              <div className="">
+                <ul className="transform translate-y-12 translate-x-32">
+                  <li className="text-white inline ml-2">
+                    <a href="#">HOME /</a>
+                  </li>
+                  <li className="text-white inline ml-2">
+                    <a href="#">PAGES /</a>
+                  </li>
+                  <li className="text-white inline ml-2 font-bold">
+                    <a href="#">ABOUT US</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-
-
-        </div>
-      
+      </div>
     </div>
   );
 }
