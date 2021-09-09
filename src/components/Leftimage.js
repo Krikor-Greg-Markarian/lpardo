dc45e0f9b5cf8ef41d66362f2274178697c95740
rony <rriachi@bright-lab.com>
2021-09-09 10:23:34 +0300

    left image component with text

---
+++ src/components/Leftimage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Leftimage(props) {
+    return (
+        <div>
+            <div className="grid grid-cols-2">
+          <div className="leftimage"></div>
+          <div className="pl-16 pt-6">
+            <div className="text-sm text-gray-500 mt-2 ">--our mission</div>
+            <p className="font-bold text-2xl mt-2 mb-2">Who We are ?</p>
+            <p className="text-sm">
+              who's the goto on this job whith the way forward waste of resource
+              shelfware, so closer to the metal enough to wash your face for UX
+              put a record on and see who dances. Synergestic actionables
+              marketing computer development html roi feedback team website.{" "}
+            </p>
+            <button className="border-2 font-bold px-4 py-1 mt-6">
+              Read More
+            </button>
+          </div>
+        </div> 
+        </div>
+    )
+}
+
+export default Leftimage
