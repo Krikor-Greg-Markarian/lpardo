d1fa085406e8840689b4ac663218f5f99f150d16
rony <rriachi@bright-lab.com>
2021-09-09 10:23:17 +0300

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 } from "react-icons/fa";
 import Bestdesigncomp from "./components/Bestdesigncomp";
 import Smallimg from "./components/Smallimg";
+import Leftimage from "./components/Leftimage";
 
 const object = {
   data: [
@@ -92,22 +93,7 @@ function App() {
       </section>
 
       <section className="mb-20">
-        <div className="grid grid-cols-2">
-          <div className="leftimage"></div>
-          <div className="pl-16 pt-6">
-            <div className="text-sm text-gray-500 mt-2 ">--our mission</div>
-            <p className="font-bold text-2xl mt-2 mb-2">Who We are ?</p>
-            <p className="text-sm">
-              who's the goto on this job whith the way forward waste of resource
-              shelfware, so closer to the metal enough to wash your face for UX
-              put a record on and see who dances. Synergestic actionables
-              marketing computer development html roi feedback team website.{" "}
-            </p>
-            <button className="border-2 font-bold px-4 py-1 mt-6">
-              Read More
-            </button>
-          </div>
-        </div>
+        <Leftimage />
       </section>
 
       <section>
@@ -162,7 +148,7 @@ function App() {
       <section className="pt-8">
         <div className=" ">
           <div className="grid grid-cols-6 gap-4">
-            <div className = "col-span-1"></div>
+            <div className="col-span-1"></div>
             <div className="col-span-3">
               <div className="grid grid-cols-3 gap-4">
                 <div className="col-span-1">
